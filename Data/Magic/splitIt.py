91689a75f3bc321f4d3794df87ad538838abd7e1
Ralf Rothenberger <ralf.rothenberger@hpi.de>
2022-06-20 03:23:46 +0200

    upgrade!

--- Data/Magic/splitIt.py
+++ Data/Magic/splitIt.py
@@ -1,10 +1,10 @@
-name="texts2"
+name="c"
 
 f=open(name+'.txt','r')
 s=f.readline()
 i=1
 while(len(s)>0):
-        f2=open(name+'/'+str(i),'w')
+        f2=open('../../'+name+'/'+str(i),'w')
         f2.write(s)
         f2.close()
         s=f.readline()
